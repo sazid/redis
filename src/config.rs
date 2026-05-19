8015ec453e81a541a33f5d693885aa6bfe1a3f12
Mohammed Sazid Al Rashid <sazidozon@gmail.com>
2026-05-19 12:24:15 +0600

    Initial commit

---
+++ src/config.rs
@@ -0,0 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Config {
+    /// Port to listen for incoming connections
+    #[arg(short, long, default_value_t = 6379)]
+    pub port: i32,
+    /// Host to listen for incoming connections
+    #[arg(long, default_value = "127.0.0.1")]
+    pub host: String,
+}
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+mod config;
+
+use clap::Parser;
+use config::Config;
+
+pub fn run() {
+    let config = Config::parse();
+
+    let host = config.host;
+    let port = config.port;
+    println!("Host: {host}");
+    println!("Host: {port}");
+}
